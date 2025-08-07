db29b0a52a888956dcbab512ce1077c7379227c3
rajnish.oss <rajnishpandey844@gmail.com>
2025-08-07 22:17:59 +0530

    m5

--- app/component/Page1.jsx
+++ app/component/Page1.jsx
@@ -328,8 +328,8 @@ const Page1 = () => {
                 <div className="hymCircle border-r-3 border-l-3 border-white/5 rounded-full flex justify-center items-center h-[20rem] w-[20rem]">
                   <img
                     
-                    src="/me1.png"
-                    className="relative h-[15rem]   bottom-3 mask- rounded-full"
+                    src="/me.png"
+                    className="relative h-[19rem]   bottom-7 mask- rounded-full"
                     alt=""
                   />
                 </div>
