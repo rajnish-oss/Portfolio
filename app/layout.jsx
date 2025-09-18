7d6c7ed1a6f23da42888b8420e36629d25205cff
rajnish-oss <rajnishpandey844@gmail.com>
2025-09-18 13:41:17 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} flex justify-center items-center antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} bg-[#010101] flex justify-center items-center antialiased`}
       >
         {children}
         <Analytics />
